4621437d3f1fdcb3a403ab809d39cb79f1829489
Jhordy272 <jaagatonp@udistrital.edu.co>
2025-05-26 12:13:34 -0500

    Format document and comment compute_public

--- main.tf
+++ main.tf
@@ -10,29 +10,31 @@ resource "aws_internet_gateway" "igw" {
 
 # Call the network module
 module "network" {
-    source = "./modules/network"
-    vpc_id = aws_vpc.vpc.id
-    internet_gateway_id = aws_internet_gateway.igw.id
+  source              = "./modules/network"
+  vpc_id              = aws_vpc.vpc.id
+  internet_gateway_id = aws_internet_gateway.igw.id
 }
 
 # Call the compute module
 module "compute" {
-    source = "./modules/compute"
-    aws_subnet_id = module.network.private_subnet_id
-    aws_security_group_ids = module.network.security_groups_ids
-    instance_count = 2
+  depends_on             = [module.network]
+  source                 = "./modules/compute"
+  aws_subnet_id          = module.network.private_subnet_id
+  aws_security_group_ids = module.network.security_groups_ids
+  instance_count         = 2
 }
 
-module "compute_public" {
-  source = "./modules/compute"
-  aws_subnet_id = module.network.public_subnet_id
-  aws_security_group_ids = module.network.security_groups_ids
-  instance_count = 1
-}
+# module "compute_public" {
+#   depends_on             = [module.network]
+#   source                 = "./modules/compute"
+#   aws_subnet_id          = module.network.public_subnet_id
+#   aws_security_group_ids = module.network.security_groups_ids
+#   instance_count         = 1
+# }
 
 # Attach the EC2 instances to the ELB
 resource "aws_elb_attachment" "elb_attachment" {
-  count       = length(module.compute.instance_ids)
-  elb         = module.network.elb_name
-  instance    = module.compute.instance_ids[count.index]
+  count    = length(module.compute.instance_ids)
+  elb      = module.network.elb_name
+  instance = module.compute.instance_ids[count.index]
 }
